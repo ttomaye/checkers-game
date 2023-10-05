17b67816ae640aaf6028ec58549a808f0745ba0f
Tommy Tomaye <tommy.tomaye@outlook.com>
2023-10-05 00:00:18 -0700

    creating cell component

--- src/board.js
+++ src/board.js
@@ -3,7 +3,6 @@ import Cell from './Cell';
 
 const Board = () => {
     const boardSize = 8;
-    const [currentPlayer, setCurrentPlayer] = useState('player1');
 
     const createBoard = () => {
         let board = [];
@@ -25,25 +24,56 @@ const Board = () => {
         return null;
     };
 
-    const handleMoveChecker = (fromRow, fromCol, toRow, toCol) => {
+    const [currentPlayer, setCurrentPlayer] = useState('player1');
+    const [initialBoard, setInitialBoard] = useState(createBoard());
+
+    const handleMoveChecker = (fromPosition, toPosition) => {
+
+        const [fromRow, fromCol] = fromPosition;
+        const [toRow, toCol] = toPosition;
+
+
+        let newBoard = [...initialBoard];
+        for (let i = 0; i < newBoard.length; i++) {
+            newBoard[i] = [...newBoard[i]];
+        }
+
+        newBoard[toRow][toCol] = newBoard[fromRow][fromCol];
+        newBoard[fromRow][fromCol] = null;
+
+        setInitialBoard(newBoard);
+
         setCurrentPlayer(currentPlayer === 'player1' ? 'player2' : 'player1');
     };
 
-    const initialBoard = createBoard();
-
     return (
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
             {initialBoard.map((row, rowIndex) => (
-                <div key={rowIndex} style={{display: 'flex', border: '1px solid black', width: '400px'}}>
+                <div key={rowIndex} style={{ display: 'flex', border: '1px solid black', width: '400px' }}>
                     {row.map((cell, cellIndex) => (
-                        <Cell
-                            key={cellIndex}
-                            color={cell.color}
-                            player={cell.player}
-                            position={[rowIndex, cellIndex]}
-                            onMoveChecker={handleMoveChecker}
-                        />
+                        cell ? (
+                            <Cell
+                                key={cellIndex}
+                                color={cell.color}
+                                player={cell.player}
+                                position={[rowIndex, cellIndex]}
+                                onDropChecker={handleMoveChecker}
+                            />
+                        ) : (
+                            <div
+                                key={cellIndex}
+                                style={{
+                                    width: '50px',
+                                    height: '50px',
+                                    backgroundColor: (rowIndex + cellIndex) % 2 === 0 ? 'white' : 'black',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            />
+                        )
                     ))}
+
                 </div>
             ))}
         </div>
